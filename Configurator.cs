ccd766f12c1224556c509fb1c102a5ad48ba4efd
Arco Mul <me@arcomul.nl>
2018-02-19 23:17:57 +0100

    Merge json files + get values by dot notation + better explanation in README

--- Configurator.cs
+++ Configurator.cs
@@ -1,16 +1,23 @@
+using System;
 using System.IO;
 using SimpleJSON;
 using UnityEngine;
+using System.Linq;
 
 public class Configurator
 {
     private static string path;
+    private static JSONNode json;
 
     public static void Initialize(string _path, string config)
     {
         path = _path;
-        JSONNode json = LoadConfig(config);
-        Debug.Log(json);
+        json = LoadConfig(config);
+    }
+
+    public static JSONNode GetJson()
+    {
+        return json;
     }
 
     private static JSONNode LoadConfig(string config)
@@ -20,43 +27,41 @@ public class Configurator
         reader.Close();
 
         JSONNode json = JSON.Parse(jsonstring);
+        JSONNode result;
 
         if (json["_extends"] != null)
         {
             JSONNode parent = LoadConfig(json["_extends"]);
-            json = Assign(parent, json);
+            result = Assign(parent, json);
+        }
+        else
+        {
+            result = json.AsObject;
         }
 
-        return json;
+        return result;
     }
 
     private static JSONNode Assign(JSONNode destination, JSONNode source)
     {
-        Debug.Log("DESTINATION: " + destination.ToString());
-        Debug.Log("SOURCE: " + source.ToString());
-
-        JSONNode json = new JSONObject();
+        JSONNode json = source;
         foreach (string key in destination.Keys)
         {
-            Debug.Log("Key: " + key);
-
-            // if (destination[key].IsObject)
-            // {
-            //     if (source[key].IsObject)
-            //     {
-            //         json[key] = Assign(destination[key].AsObject, source[key].AsObject);
-            //     }
-            //     else
-            //     {
-            //         json[key] = Pick(source[key], destination[key]);
-            //     }
-            // }
-
-            Debug.Log("source: " + source[key].ToString());
-            Debug.Log("destination: " + destination[key].ToString());
-
-            json[key] = Pick(source[key], destination[key]);
-            Debug.Log("json[key]: " + json[key].ToString());
+            if (destination[key].IsObject)
+            {
+                if (source[key].IsObject)
+                {
+                    json[key] = Assign(destination[key].AsObject, source[key].AsObject);
+                }
+                else
+                {
+                    json[key] = Pick(source[key], destination[key]);
+                }
+            }
+            else
+            {
+                json[key] = Pick(source[key], destination[key]);
+            }
         }
         return json;
     }
@@ -69,4 +74,75 @@ public class Configurator
         }
         return second;
     }
+
+    private static JSONNode GetJSONNode(string path)
+    {
+        string[] paths = path.Split('.');
+        JSONNode depth = json;
+        foreach (string p in paths)
+        {
+            if (depth[p] == null)
+            {
+                return null;
+            }
+            depth = depth[p];
+        }
+        return depth;
+    }
+
+    public static string Get(string path)
+    {
+        return GetJSONNode(path).ToString();
+    }
+    public static int GetInt(string path)
+    {
+        return GetJSONNode(path);
+    }
+    public static bool GetBool(string path)
+    {
+        return GetJSONNode(path).AsBool;
+    }
+    public static float GetFloat(string path)
+    {
+        return GetJSONNode(path).AsFloat;
+    }
+    public static double GetDouble(string path)
+    {
+        return GetJSONNode(path).AsDouble;
+    }
+    public static JSONObject GetObject(string path)
+    {
+        return GetJSONNode(path).AsObject;
+    }
+
+    public static JSONArray GetArray(string path)
+    {
+        JSONNode n = GetJSONNode(path);
+        if (n == null)
+        {
+            return null;
+        }
+        return n.AsArray;
+    }
+    public static string[] GetStringArray(string path)
+    {
+        return GetArray(path).Children.Select(node => node.ToString()).ToArray();
+    }
+    public static int[] GetIntArray(string path)
+    {
+        return GetArray(path).Children.Select(node => node.AsInt).ToArray();
+    }
+    public static bool[] GetBoolArray(string path)
+    {
+        return GetArray(path).Children.Select(node => node.AsBool).ToArray();
+    }
+    public static float[] GetFloatArray(string path)
+    {
+        return GetArray(path).Children.Select(node => node.AsFloat).ToArray();
+    }
+    public static double[] GetDoubleArray(string path)
+    {
+        return GetArray(path).Children.Select(node => node.AsDouble).ToArray();
+    }
+
 }
--- README.md
+++ README.md
@@ -1,6 +1,67 @@
 # Configurator
 
-** WIP: not stable at all **
+Read from extendable config files.
 
-Define extendable configs in json and easily read values in your C# application.
+## Usage
 
+Get values from JSON file by specifying the key. Seperate levels with a dot. See the Example section below on how to extend one config with another.
+
+```C#
+// Define path to directory with all config files and the config you'd like to read
+#if UNITY_STANDALONE
+Configurator.Initialize(Directory.GetParent(Application.dataPath).FullName + @"\Configs", "production");
+#else
+Configurator.Initialize(Directory.GetParent(Application.dataPath).FullName + @"\Configs", "editor");
+#endif
+
+// Read values
+string apiKey = Configurator.Get("api.key"));
+float defaultVolume = Configurator.GetFloat("audio.defaults.volume"));
+string[] tags = Configurator.GetStringArray("tags"));
+bool sendEmail = Configurator.GetBool("email.enabled"));
+```
+
+## Example
+
+**Common.json**
+
+```json
+{
+    "title": "Generic application",
+    "version": "0.1",
+    "api": {
+        "host": "http://localhost:3000",
+        "path": "/api/v1",
+        "key": "123",
+        "secret: "456"
+    }
+}
+```
+
+**Production.json**
+
+```json
+{
+    "_extends": "Common",
+    "api": {
+        "host": "https://production.com",
+        "key": "abc",
+        "secret: "def"
+    }
+}
+```
+
+**Resulting config**
+
+```json
+{
+    "title": "Generic application",
+    "version": "0.1",
+    "api": {
+        "host": "https://production.com",
+        "path": "/api/v1",
+        "key": "abc",
+        "secret: "def"
+    }
+}
+```
